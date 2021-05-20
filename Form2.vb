ccc08c9cd5c461a9de9e060cf7cfb43f28aa0341
WySuKo <74152865+WySuKo@users.noreply.github.com>
2021-05-20 09:16:37 +0200

    Enfffiiiiinnn !

--- Form2.vb
+++ Form2.vb
@@ -5,7 +5,7 @@ Public Class Form2
     Dim stockImage(5) As Image
     Dim cpt0 As Integer, cpt1 As Integer, cpt2 As Integer, cpt3 As Integer, cpt4 As Integer
     Dim cptCarte As Integer
-    Dim tabImage(3) As Integer
+    Dim tabImage As ArrayList = New ArrayList()
     Dim Start As Boolean = False
     Dim carteClicked As Integer = 0
     Dim seconde As Integer = 1000
@@ -14,11 +14,11 @@ Public Class Form2
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LabelPseudo.Text = StringPassage
         Timer1.Interval = seconde
-        stockImage(0) = My.Resources.Image_2
-        stockImage(1) = My.Resources.Image_3
-        stockImage(2) = My.Resources.Image_4
-        stockImage(3) = My.Resources.Image_0
-        stockImage(4) = My.Resources.Image_1
+        stockImage(0) = My.Resources.Image_0
+        stockImage(1) = My.Resources.Image_1
+        stockImage(2) = My.Resources.Image_2
+        stockImage(3) = My.Resources.Image_3
+        stockImage(4) = My.Resources.Image_4
         stockImage(5) = My.Resources.Image_5
 
         'Randomize()
@@ -88,7 +88,6 @@ Public Class Form2
         End While
         bool = True
     End Function
-
     Private Sub BtnAbandon_Click(sender As Object, e As EventArgs) Handles BtnAbandon.Click
         If MsgBox("Etes-vous sur de vouloir quitter ?", vbYesNo, "Abandon") = vbYes Then
             Me.Close()
@@ -96,7 +95,6 @@ Public Class Form2
         End If
 
     End Sub
-
     Private Sub Click_Label(sender As Object, e As EventArgs)
         If Start = False Then
             Timer1.Start()
@@ -105,32 +103,35 @@ Public Class Form2
 
         sender.Image = stockImage(sender.Tag)
         sender.Image.Tag = 1
-        If sender.Image.Tag = 1 And carteClicked >= 1 Then
-            Threading.Thread.Sleep(seconde)
-            tabImage(carteClicked) = sender.Tag
+        If sender.Image.Tag = 1 And carteClicked >= 0 And carteClicked <= 4 Then
+            tabImage.Add(sender.Tag)
+            If carteClicked >= 1 And carteClicked <= 4 Then
+                Verif_Tableau()
+            End If
         End If
         carteClicked += 1
-        Verif_Tableau()
-    End Sub
 
+    End Sub
     Private Sub Verif_Tableau()
-        For j = 0 To tabImage.Length - 1
-            For k = j + 1 To tabImage.Length - 1
-                If tabImage(j) <> tabImage(k) Then
-                    Retourner_Carte()
-                    Exit Sub
-                End If
-            Next
+        Dim size As Integer = tabImage.Count - 2
+        For j = 0 To size
+            If tabImage(j) <> tabImage(j + 1) Then
+                Retourner_Carte()
+                Exit Sub
+            End If
         Next
     End Sub
     Private Sub Retourner_Carte()
+        Refresh()
+        Threading.Thread.Sleep(seconde)
         For Each Label As Label In Panel1.Controls
-            If Label.Image.Tag = 1 And Label.Enabled <> False Then
+            If Label.Image.Tag = 1 Then
                 Label.Image = stockImage(5)
                 Label.Image.Tag = 0
             End If
         Next
         carteClicked = 0
+        tabImage.Clear()
     End Sub
     Private Sub Add_Event()
         For Each label As Label In Panel1.Controls
